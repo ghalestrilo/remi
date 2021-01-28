a08c998873a2475af2ab7254499dabdcf59426c0
ghalestrilo <ghalestrilo@gmail.com>
2021-01-28 12:33:10 -0300

    :construction: create engine history

--- server.py
+++ server.py
@@ -18,8 +18,11 @@ import math
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+# TODO: Move to engine.py
+
 state = {
-  'isRunning': False
+  'isRunning': False,
+  'history': []
 }
 
 def print_volume_handler(unused_addr, args, volume):
@@ -30,8 +33,41 @@ def print_compute_handler(unused_addr, args, volume):
     print("[{0}] ~ {1}".format(args[0], args[1](volume)))
   except ValueError: pass
 
-def engine_start():
-  state['isRunning'] = True
+def engine_set(unused_addr, args):
+  try:
+    field, value = args
+    state[field] = value
+    print("[{0}] ~ {1}".format(field, value))
+  except KeyError:
+    print("no such key ~ {0}".format(field))
+    pass
+
+def push_event(unused_addr, event):
+  print("[event] ~ {0}".format(event))
+  state['history'].append(event)
+
+def engine_print(unused_addr, args):
+  field = args
+  try:
+    print("[{0}] ~ {1}".format(field, state[field]))
+  except KeyError:
+    print("no such key ~ {0}".format(field))
+    pass
+
+def bind_dispatcher(dispatcher):
+  dispatcher.map("/filter", print)
+  dispatcher.map("/volume", print_volume_handler, "Volume")
+  dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+
+  dispatcher.map("/start", engine_set, 'isRunning', True)
+  dispatcher.map("/pause", engine_set, 'isRunning', False)
+  dispatcher.map("/reset", engine_set, 'history',   list([]))
+
+  dispatcher.map("/debug", engine_print)
+  dispatcher.map("/event", push_event)
+# /TODO: Move to engine.py
+
+
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
@@ -42,11 +78,7 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   dispatcher = dispatcher.Dispatcher()
-  dispatcher.map("/filter", print)
-  dispatcher.map("/volume", print_volume_handler, "Volume")
-  dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
-
-  dispatcher.map("/start", engine_start)
+  bind_dispatcher(dispatcher)
 
   server = osc_server.ThreadingOSCUDPServer(
       (args.ip, args.port), dispatcher)
